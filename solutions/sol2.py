e252be45c36dbc9c938a72d4f9bf4d9d32d62b95
Shreyas Karnik <skarnik@rmn.com>
2014-04-23 14:37:19 -0500

    part 2 wip

--- solutions/sol2.py
+++ solutions/sol2.py
@@ -4,3 +4,12 @@ weather_df = pd.DataFrame(X)
 weather_df = weather_df.sort(['humidity', 'temprature'], ascending=False)
 weather_df
 
+webdata_df.kmeans_labels.value_counts().plot(kind='barh')
+
+from IPython.display import Image
+i = Image(filename='data/clustering_kmeans.png')
+i
+
+from IPython.display import Image
+i = Image(filename='data/clustering_dbscan.png')
+i
